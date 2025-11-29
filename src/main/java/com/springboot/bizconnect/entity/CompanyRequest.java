bb36935b0db7fc8299a2af4776dff652f0e96839
이성훈 <dgh0612321@naver.com>
2025-11-29 21:47:49 +0900

    추가 엔티티 추가

---
+++ src/main/java/com/springboot/bizconnect/entity/CompanyRequest.java
@@ -0,0 +1,54 @@
+package com.springboot.bizconnect.entity;
+
+import com.springboot.bizconnect.enums.requestStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "company_request")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CompanyRequest extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "company_request_no", nullable = false)
+    private Long companyRequestNo;
+
+    @Column(name = "address", nullable = false, length = 100)
+    private String address;
+
+    @Column(name = "phone_number", nullable = false, length = 30)
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status",nullable = false)
+    private requestStatus status;
+
+    @OneToOne
+    @JoinColumn(name = "user_no", nullable = false)
+    private User user;
+
+    @OneToOne
+    @JoinColumn(name = "affiliation_no", nullable = false)
+    private Affiliation affiliation;
+
+    @OneToOne
+    @JoinColumn(name = "branch_no", nullable = false)
+    private Branch branch;
+}
