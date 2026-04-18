4f9cb2f46123bc91496c49be83f3b97689271fd2
이성훈 <dgh0612321@naver.com>
2026-04-18 13:27:28 +0900

    test

--- src/main/java/com/springboot/bizconnect/domain/order/service/OrderService.java
+++
@@ -1,4 +0,0 @@
-package com.springboot.bizconnect.domain.order.service;
-
-public interface OrderService {
-}
--- src/main/java/com/springboot/bizconnect/domain/order/service/impl/OrderServiceImpl.java
+++
@@ -1,6 +0,0 @@
-package com.springboot.bizconnect.domain.order.service.impl;
-
-import com.springboot.bizconnect.domain.order.service.OrderService;
-
-public class OrderServiceImpl implements OrderService {
-}
